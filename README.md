45de20db1bf88e1923771b760d2083d95575fe7a
mauvm <maurits@nerdieworks.nl>
2014-06-10 11:46:22 +0200

    Minimal working example.

---
+++ README.md
@@ -0,0 +1,14 @@
+# Documark &lt;hr/&gt; &rarr; page-break
+
+### Usage
+
+1. Add plugin to document configuration:
+
+        plugins:
+          - hr-to-page-break
+
+2. Add horizontal rules for forced page-breaks:
+
+	- `<hr/>` in HTML;
+	- `hr` in Jade;
+	- `---` in Markdown.
---
+++ index.js
@@ -0,0 +1,12 @@
+/* jslint node: true */
+
+function hrToPageBreak( document ) {
+	document.on( 'plugin:html', function( plugins ) {
+		plugins.push( function( data, cb ) {
+			data = data.replace( /<hr[^>]*>/gi, '<div style="page-break-after:always"></div>' );
+			cb( null, data );
+		} );
+	} );
+}
+
+module.exports = hrToPageBreak;
