509b44eee3134b026459a2a47cdbe680f078a3b3
mauvm <maurits@nerdieworks.nl>
2014-06-11 14:16:18 +0200

    Ignore horizontal rules with attributes.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 function hrToPageBreak( document ) {
 	document.on( 'plugin:html', function( plugins ) {
 		plugins.push( function( data, cb ) {
-			data = data.replace( /<hr[^>]*>/gi, '<div style="page-break-after:always"></div>' );
+			data = data.replace( /<hr[\s]*\/?>/gi, '<div style="page-break-after:always"></div>' );
 			cb( null, data );
 		} );
 	} );
